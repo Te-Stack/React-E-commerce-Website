ae132f4aed0c8d0cefcad721d2728df53716c54a
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-07 15:52:44 +0100

    Working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles/index.scss"
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl text-bold">E commerce</h1>
+      <p className="text-5xl font-bold">E commerce</p>
     </div>
   );
 }
