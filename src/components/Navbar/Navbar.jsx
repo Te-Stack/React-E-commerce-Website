13c648722dbf789bbc9867146906c6c6a5a9649a
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-11 11:37:41 +0100

    Working

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,50 @@
+import { useState } from "react";
+
+import { FaBars, FaTimes } from "react-icons/fa";
+import  "./navbar.scss"
+
+
+
+
+
 const Navbar = () => {
+    const [active, setActive] = useState(false);
+
+    const toggleLinks = () => {
+        setActive(!active);
+      };
+    
+
     return ( 
-        <div>
+        <>
+            <nav className="NavbarItems">
+            <Link href="/"><img className="styles.navbarlogo" src={Logo} /></Link>
+            <div className="styles.menuicon" onClick={toggleLinks}> 
+            {active ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={active ? "navmenu active": "navmenu"}>
+                <li>
+                    <Link href="#"><a className={styles.navlinks}>Pricing</a></Link>
+                </li>
+                <li>
+                    <Link  href="#"><a className={styles.navlinks}>Product</a></Link>
+                </li> 
+                <li>
+                    <Link href="#"><a className={styles.navlinks}>About Us</a></Link>
+                </li>
+                <li>
+                    <Link href="#"><a className={styles.navlinks}>Careers</a></Link>
+                </li>
+                <li>
+                    <Link href="#"><a className={styles.navlinks}>Community</a></Link>
+                </li>      
+            </ul>
+            <Button text={"Get Started"} />
+
             
-        </div>
+        </nav>
+            
+        </>
      );
 }
  
