8b97abe2b5e238b43155e009ca5dde9e211d6b42
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-12 14:03:34 +0100

    Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import "./styles/index.scss"
+import "./styles/index.scss";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      
       <p className="text-5xl font-bold">E commerce1</p>
     </div>
   );
