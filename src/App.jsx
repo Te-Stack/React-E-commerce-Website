6e04bc8a81f773c327f12f2467601c94ea0c5c11
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-13 07:25:14 +0100

    Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter , Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter , Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
