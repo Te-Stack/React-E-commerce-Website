05242f4f8175c13ad178155ee38b410d345aafb4
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-13 06:02:27 +0100

    Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { BrowserRouter , Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/Home";
+import Cart from "./pages/Home";
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
+    <div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/" element={< Home />} />
+        <Route path="/" element={< Home />} />
+
+
+        </Routes>
+      </BrowserRouter>
       
-      <p className="text-5xl font-bold">E commerce2</p>
     </div>
   );
 }
