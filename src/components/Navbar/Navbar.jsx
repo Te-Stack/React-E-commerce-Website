393b1ae826fdf1ee2ac21df7e9f8aa812ff52f4f
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-13 09:33:21 +0100

    Working

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -42,9 +42,13 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <Link to="/"><h1 className="navbarlogo text-3xl "  > PAYNA</h1></Link>
-                <div className="Search">
-                <AiOutlineSearch/>
-                <Link to="/Cart" ><AiOutlineShoppingCart/></Link>
+                    <div className="Search">
+                    <p>English <span>/italy</span></p>
+                    <div className="icons">
+                        < AiOutlineSearch/>
+                    </div>
+                    
+                    <Link className="icons" to="/Cart" ><AiOutlineShoppingCart/></Link>
                 </div>
                 
                    
