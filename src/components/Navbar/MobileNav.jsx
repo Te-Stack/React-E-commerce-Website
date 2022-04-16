c2548d091d5aae7b087b8ac5cb2e9f37a1833dc8
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-16 23:13:07 +0100

    Working

--- src/components/Navbar/MobileNav.jsx
+++ src/components/Navbar/MobileNav.jsx
@@ -9,10 +9,9 @@ import {AiOutlineDribbble} from "react-icons/ai";
 import {RiBarChartHorizontalLine} from "react-icons/ri"
 import {  FaTimes } from "react-icons/fa";
 import  "./navbar.scss";
-import MobileNav from "./MobileNav"
  
 
-const Navbar = () => {
+const MobileNav = () => {
     const [active, setActive] = useState(false);
 
     const toggleLinks = () => {
@@ -22,38 +21,19 @@ const Navbar = () => {
 
     return ( 
         <div>
-            <nav className="NavbarItems">
-                <div className="Part"> 
-                    <ul className="navmenu">
-                        <div className="menuicon" onClick={toggleLinks}> 
-                        {active ? <FaTimes/> : <RiBarChartHorizontalLine />}
-                        </div>
-                        <li>
-                            <a href="https://github.com/Te-Stack" className="navlinks"><ImFacebook/></a>
-                        </li>
-                        <li>
-                            <a className="navlinks" href="https://github.com/Te-Stack" ><BsInstagram/></a>
-                        </li> 
-                        <li>
-                            <a className="navlinks" href="https://github.com/Te-Stack"><BsGithub/></a>
-                        </li>
-                        <li>
-                            <a className="navlinks" href="https://github.com/Te-Stack"><AiOutlineDribbble/></a>
-                        </li>    
-                    </ul>
+
+            <nav className="MobileNavbarItem">
+                <Link to="/"><h1 className="navbarlogo text-3xl"  > PAYNA</h1></Link>
+                <Link className="icons"  to="/Cart" ><AiOutlineShoppingCart/></Link>
+                <div className="menuicon" onClick={toggleLinks}> 
+                {active ? <FaTimes/> : <RiBarChartHorizontalLine />}
                 </div>
-                <Link to="/"><h1 className="navbarlogo text-3xl "  > PAYNA</h1></Link>
-                    <div className="Search">
-                    <p>English <span>/italy</span></p>
-                    <div className="icons">
-                        < AiOutlineSearch/>
+                <ul className={active ? "navmenu active": "navmenu"}>
+                    <div>
+                        <input type="text" placeholder="Search entire store...." />< AiOutlineSearch/>
                     </div>
-                    
-                    <Link className="icons" to="/Cart" ><AiOutlineShoppingCart/></Link>
-                </div>        
-            </nav>
-            <div className={active? "broad active" :"broad"}>
-                            <div>
+                    <div>
+                    <div>
                                 <h1>Home</h1>
                                 <p>Demo Group 01</p>
                                 <p>Demo Group 02</p>
@@ -82,12 +62,25 @@ const Navbar = () => {
                                 <p>Single Post</p>
                             </div>
 
-                        </div>
-
-            <MobileNav/>
+                    </div>
+                    
+                    <li>
+                        <a href="https://github.com/Te-Stack" className="navlinks"><ImFacebook/></a>
+                    </li>
+                    <li>
+                        <a className="navlinks" href="https://github.com/Te-Stack"><BsInstagram/></a>
+                    </li> 
+                    <li>
+                        <a className="navlinks" href="https://github.com/Te-Stack"><BsGithub/></a>
+                    </li>
+                    <li>
+                        <a className="navlinks" href="https://github.com/Te-Stack"><AiOutlineDribbble/></a>
+                    </li>    
+                </ul>
+            </nav>
             
         </div>
      );
 }
  
-export default Navbar;
+export default MobileNav;
