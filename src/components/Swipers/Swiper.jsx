c4d570e62c9e4c76967d03f8d99cbdd0165e3c0f
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-20 23:38:15 +0100

    Working

--- src/components/Swipers/Swiper.jsx
+++ src/components/Swipers/Swiper.jsx
@@ -1,22 +1,52 @@
-import { useTransitionCarousel, useSpringCarousel } from 'react-spring-carousel'
+import { useTransitionCarousel } from 'react-spring-carousel'
+import  { useEffect } from 'react';
+import "./swiper.scss"
+
 
 
 const Swiper = () => {
-    const { carouselFragment } = useSpringCarousel({
-        items: mockedItems.map((i) => ({
-          id: i.id,
-          renderItem: (
-            <CarouselItem color={i.color}>
-              {i.title}
-            </CarouselItem>
-          ),
-        })),
+    const { 
+        carouselFragment, 
+        slideToPrevItem, 
+        slideToNextItem 
+      } = useTransitionCarousel({
+        items: [
+            {
+                id: "1",
+                renderItem: (
+                  <div className="items">Item 1</div>
+                )
+            },
+            {
+                id: "2",
+                renderItem: (
+                  <div className="items">Item 2</div>
+                )
+            },
+            {
+                id: "3",
+                renderItem: (
+                  <div className="items" >Item 3</div>
+                )
+            }
+        ]
       });
+
+     useEffect(() => {
+        const timer = setInterval(() => {
+          slideToNextItem();
+        }, 1500);
+        return () => {
+          window.clearInterval(timer);
+        };
+        // You MUST add the slide methods to the dependency list useEffect!
+      }, [slideToNextItem,slideToPrevItem]);
   
     return ( 
-    
-    <div>
-         {carouselFragment}
+      <div className="swiper">
+        <button onClick={slideToPrevItem}>Prev item</button>
+        {carouselFragment}
+        <button onClick={slideToNextItem}>Next item</button>
       </div>
      );
 }
