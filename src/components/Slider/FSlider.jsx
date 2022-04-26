3d038439fd766bf278d7d73ecaa2c5a02a1c2bb2
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-26 17:56:24 +0100

    Working

--- src/components/Slider/FSlider.jsx
+++ src/components/Slider/FSlider.jsx
@@ -1,9 +1,12 @@
 import { Component } from "react";
 import "./fslider.scss";
+import { Link } from "react-router-dom";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-
+import img3 from "../../assets/images/product/category-1.jpg";
+import img4 from "../../assets/images/product/category-2.jpg";
+import img5 from "../../assets/images/product/category-3.jpg";
 
 
 
@@ -46,24 +49,32 @@ export default class Carousels extends Component {
       return (
         <div className="carousel">
           <Slider {...settings}>
-            <div className="carouselCard">
-                <div>
-                  Men's Dress
-                </div>
-            </div>
-
-            <div  className="carouselCard2">
-              <div>
-                Women's Dress
-              </div>
-            </div>
-
-            <div  className="carouselCard3">
-
-                <div>
-                  Kids
-                </div>
-            </div>
+            
+            <div className="category-wrap">
+              <link to="/ProductDetail" ><img src={img3} alt="product"/></link>
+              <div className="category-content">
+                    <h4><Link to="/ProductDetail">Men Jacket</Link></h4>
+                 </div>
+           </div>
+            <div className="category-wrap">
+              <link to="/ProductDetail" ><img src={img4} alt="product"/></link>
+              <div className="category-content">
+                    <h4><Link to="/ProductDetail">Women Dress</Link></h4>
+                 </div>
+           </div>
+            <div className="category-wrap">
+              <link to="/ProductDetail" ><img src={img5} alt="product"/></link>
+              <div className="category-content">
+                    <h4><Link to="/ProductDetail">Kid</Link></h4>
+                 </div>
+           </div>
+            <div className="category-wrap">
+              <link to="/ProductDetail" ><img src={img3} alt="product"/></link>
+              <div className="category-content">
+                    <h4><Link to="/ProductDetail">Women Dress</Link></h4>
+                 </div>
+           </div>
+            
 
           </Slider>
 
