29daa9d92d66dd6b5a344ae48d9c6c310ece066f
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-26 01:33:39 +0100

    Working

--- src/components/Slider/FSlider.jsx
+++ src/components/Slider/FSlider.jsx
@@ -11,7 +11,7 @@ export default class Carousels extends Component {
     render() {
       var settings = {
         dots: true,
-        infinite: false,
+        infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
