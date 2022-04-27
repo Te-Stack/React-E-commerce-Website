41e39d6f95b55a36a63b8a4bc42ed618bb2a8553
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-27 10:10:35 +0100

    Working

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,14 +2,14 @@ import "./product.scss";
 const Product = () => {
     return ( 
         <div className="products">
-            <div>Coulumn1</div>
-            <div>Coulumn2</div>
-            <div>Coulumn3</div>
-            <div>Coulumn4</div>
-            <div>Coulumn5</div>
-            <div>Coulumn6</div>
-            <div>Coulumn7</div>
-            <div>Coulumn8</div>
+            <div className="div">Coulumn1</div>
+            <div className="div">Coulumn2</div>
+            <div className="div">Coulumn3</div>
+            <div className="div" >Coulumn4</div>
+            <div className="div">Coulumn5</div>
+            <div className="div">Coulumn6</div>
+            <div className="div">Coulumn7</div>
+            <div className="div">Coulumn8</div>
 
         </div>
      );
