b0c9f75be06f181aa7d159003ea52c2a2efbe6d1
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-27 09:51:03 +0100

    Working

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,15 @@
 import "./product.scss";
 const Product = () => {
     return ( 
-        <div>
-            Products
+        <div className="products">
+            <div>Coulumn1</div>
+            <div>Coulumn2</div>
+            <div>Coulumn3</div>
+            <div>Coulumn4</div>
+            <div>Coulumn5</div>
+            <div>Coulumn6</div>
+            <div>Coulumn7</div>
+            <div>Coulumn8</div>
 
         </div>
      );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Product from "../components/Products/Products";
 import Carousels from "../components/Slider/FSlider";
 import Swiper from "../components/Swipers/Swiper";
 
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Swiper/>
             <Carousels/>
+            <Product/>
         </div>
      );
 }
