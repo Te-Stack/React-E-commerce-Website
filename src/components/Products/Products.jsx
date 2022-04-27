65eb57bf696e1c55d762f73f7505b1295afd4372
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-04-27 16:43:25 +0100

    Working

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,33 @@
 import "./product.scss";
+import { Link } from "react-router-dom";
 const Product = () => {
     return ( 
         <div className="ProductArea"> 
             <div className="title text-center">
                     <h2 className="text-3xl font-bold">NEW PRODUCTS</h2>
                     <p>Show popular your products</p>
+            </div>
+            <div className="tabs">
+                    <div className="product-tab-list ">
+                        <a className="active" href="#product-1" >
+                            Man
+                        </a>
+                        <a href="#product-2" >
+                            Woman
+                        </a>
+                        <a href="#product-3" >
+                            Kids
+                        </a>
+                        <a href="#product-4" >
+                            Accessories
+                        </a>
+                        <a href="#product-5">
+                            Essential prices
+                        </a>
+                    </div>
+                    <div className="more">
+                        <Link href="/">View more</Link>
+                    </div>
             </div>
 
 
