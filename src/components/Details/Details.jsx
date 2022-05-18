f5cdc0f7d7d4bed0e74271d72da3518e9e2c3153
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-05-18 16:30:20 +0100

    Working

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,16 @@
+import "./details.scss"
+
+const Details = () => {
+    return ( 
+        <div>
+            <div className="grid grid-rows-2 md:grid-row-6">
+                <div className="w-64 bg-red-500">Box</div>
+                <div className="bg-red-500">Goat</div>
+
+            </div>
+
+        </div>
+     );
+}
+ 
+export default Details;
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,9 +1,11 @@
+import Details from "../components/Details/Details";
 import PCarousels from "../components/ProductSlider/PSlider";
 
 const ProductDetail = () => {
     return ( 
         <div>
             <h1>This is the ProductDetail Section</h1>
+            <Details/>
             <PCarousels/>
         </div>
         
