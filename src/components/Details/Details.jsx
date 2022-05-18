146ed70c9376a5a0a349bb150821d686fee05eb2
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-05-18 16:38:09 +0100

    Working

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -3,9 +3,9 @@ import "./details.scss"
 const Details = () => {
     return ( 
         <div>
-            <div className="grid grid-rows-2 md:grid-row-6">
+            <div className="grid  xl:grid-row-6">
                 <div className="w-64 bg-red-500">Box</div>
-                <div className="bg-red-500">Goat</div>
+                <div className="w-64 bg-red-500">Goat</div>
 
             </div>
 
