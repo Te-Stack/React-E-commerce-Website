c81042953cfef662af1e9144d4b45c8a7c23aa6e
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-06-01 21:48:38 +0100

    Working

---
+++ src/context/cart/CartTypes.js
@@ -0,0 +1,8 @@
+// /src/Context/Cart/CartTypes.js`:
+
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_ITEM = "REMOVE_ITEM";
+export const INCREASE = "INCREASE";
+export const DECREASE = "DECREASE";
+export const CHECKOUT = "CHECKOUT";
+export const CLEAR = "CLEAR";
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./styles/index.scss";
+import CartState from "./Context/Cart/CartState";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>)
+
+root.render(
+    <CartState>
+        <App/>
+    </CartState>
+)
