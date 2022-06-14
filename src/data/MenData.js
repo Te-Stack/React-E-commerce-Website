d3cb545649868d3fbdb9f7fece86e867db33777a
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-06-14 15:13:59 +0100

    Working

---
+++ src/data/MenData.js
@@ -0,0 +1,14 @@
+export const men = [
+    {
+      id: 56,
+      name: "Slim-fit cotton shirt",
+      price: 50.00,
+      image1: "/assets/images/product-details/b-large-1.jpg",
+      image2: "/assets/images/product-details/b-large-2.jpg",
+      image3:"/assets/images/product-details/b-large-3.jpg",
+      image4:"/assets/images/product-details/b-large-4.jpg"
+    }
+
+   
+  ];
+
