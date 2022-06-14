74eddcb3652878e817da24f5a2f0e60671c643e3
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-06-14 15:28:27 +0100

    Working

---
+++ src/data/ChildData.js
@@ -0,0 +1,13 @@
+export const child = [
+    {
+      id: 58,
+      name: "Cardigan",
+      price: 50.00,
+      image1: "/assets/images/product-details/hm-4-pro-10.jpg",
+      image2: "/assets/images/product-details/hm-4-pro-10-2.jpg",
+      image3:"/assets/images/product-details/hm-4-pro-11.jpg",
+      image4:"/assets/images/product-details/hm-4-pro-11-2.jpg"
+    }
+
+   
+  ];
---
+++ src/data/WomenData.js
@@ -0,0 +1,14 @@
+export const women = [
+    {
+      id: 57,
+      name: "Off-shoulder striped dress",
+      price: 50.00,
+      image1: "/assets/images/product-details/b-large-9.jpg",
+      image2: "/assets/images/product-details/b-large-10.jpg",
+      image3:"/assets/images/product-details/b-large-11.jpg",
+      image4:"/assets/images/product-details/b-large-12.jpg"
+    }
+
+   
+  ];
+
